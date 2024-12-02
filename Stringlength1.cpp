10fe2b1f8943ae8d81a5a1738fe8dcb16ffb3633
swati singh <singhswati8219860@gmail.com>
2024-12-02 16:04:15 +0530

    count length of string

---
+++ Stringlength1.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char s[] = "welcome";
+    int i;
+    for(i=0;s[i]!='\0';i++)
+    {
+
+    }
+    cout<<i;
+
+    return 0;
+}
